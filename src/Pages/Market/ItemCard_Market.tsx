1332dc6232280fb72cd7529ab217aecd8f239cdf
ppk212 <ppk212@naver.com>
2022-02-25 02:19:22 +0900

    [Edit] edit for logout user

--- src/Pages/Market/ItemCard_Market.tsx
+++ src/Pages/Market/ItemCard_Market.tsx
@@ -1,68 +1,73 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import React, {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router';
 import styled from 'styled-components';
-import { IItemMarketCardProps } from '../../Store/Type/Interfaces';
-import { getImage } from '../../Api';
+import {IItemMarketCardProps} from '../../Store/Type/Interfaces';
+import {getImage} from '../../Api';
+import {useRecoilValue} from "recoil";
+import {userInfoAtom} from "../../Store/Atoms";
 
 const ItemCard_Market = ({
-  id,
-  name,
-  imageURI,
-  durability,
-  maxDurability,
-  price,
-  ownerNickname,
-  setItem,
-}: IItemMarketCardProps) => {
-  const navigate = useNavigate();
-  const [image, setImage] = useState('');
+                             id,
+                             name,
+                             imageURI,
+                             durability,
+                             maxDurability,
+                             price,
+                             ownerNickname,
+                             setItem,
+                         }: IItemMarketCardProps) => {
+    const navigate = useNavigate();
+    const userInfo = useRecoilValue(userInfoAtom);
+    const [image, setImage] = useState('');
 
-  useEffect(() => {
-    console.log('id changed', imageURI);
-    getImage(imageURI)
-      .then((res) => {
-        setImage(res);
-      })
-      .catch(() => setImage(''));
-  }, [id]);
+    useEffect(() => {
+        console.log('id changed', imageURI);
+        getImage(imageURI)
+            .then((res) => {
+                setImage(res);
+            })
+            .catch(() => setImage(''));
+    }, [id]);
 
-  return (
-    <Container>
-      <div className="image">
-        <img
-          src={image}
-          style={{
-            width: '100%',
-            height: '100%',
-          }}
-        />
-      </div>
-      <div className="content">
-        <h4 className="title">{name}</h4>
-        <span className="dutability">
+    return (
+        <Container>
+            <div className="image">
+                <img
+                    src={image}
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                    }}
+                />
+            </div>
+            <div className="content">
+                <h4 className="title">{name}</h4>
+                <span className="dutability">
           {durability}/{maxDurability}회 남음
         </span>
-        <span className="price">{price} KLAY</span>
-      </div>
-      <div
-        className="buy_btn"
-        onClick={() => {
-          setItem({
-            id: id,
-            name: name,
-            imageURI: imageURI,
-            durability: durability,
-            maxDurability: maxDurability,
-            price: price,
-            ownerNickname: ownerNickname,
-          });
-          navigate('/market/buying');
-        }}
-      >
-        구입
-      </div>
-    </Container>
-  );
+                <span className="price">{price} KLAY</span>
+            </div>
+            {userInfo.accessToken != '' &&
+            <div
+                className="buy_btn"
+                onClick={() => {
+                    setItem({
+                        id: id,
+                        name: name,
+                        imageURI: imageURI,
+                        durability: durability,
+                        maxDurability: maxDurability,
+                        price: price,
+                        ownerNickname: ownerNickname,
+                    });
+                    navigate('/market/buying');
+                }}
+            >
+                구입
+            </div>
+            }
+        </Container>
+    );
 };
 
 export default ItemCard_Market;
@@ -73,26 +78,26 @@ const Container = styled.article`
   grid-column-gap: 14px;
   height: 100px;
   padding: 12px 11px;
-  ${({ theme }) => theme.mixin.textStyle.R_12}
+  ${({theme}) => theme.mixin.textStyle.R_12}
   background-color: #24253a;
   border-radius: 6px;
   position: relative;
   .image {
     width: 77px;
     height: 77px;
-    background-color: ${({ theme }) => theme.variable.colors.A_FFF};
+    background-color: ${({theme}) => theme.variable.colors.A_FFF};
     border-radius: 5px;
   }
   .content {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    color: ${({ theme }) => theme.variable.colors.A_FFF};
+    color: ${({theme}) => theme.variable.colors.A_FFF};
     .title {
-      ${({ theme }) => theme.mixin.flexCenter}
+      ${({theme}) => theme.mixin.flexCenter}
       padding: 0 9px;
       height: 20px;
-      background-color: ${({ theme }) => theme.variable.colors.item_color};
+      background-color: ${({theme}) => theme.variable.colors.item_color};
       border-radius: 10px;
     }
     .dutability {
@@ -100,7 +105,7 @@ const Container = styled.article`
       color: #a2a4b7;
     }
     .price {
-      ${({ theme }) => theme.mixin.textStyle.R_12}
+      ${({theme}) => theme.mixin.textStyle.R_12}
       margin-top: 20px;
     }
   }
@@ -110,7 +115,7 @@ const Container = styled.article`
     right: 0;
     width: 50px;
     height: 100%;
-    ${({ theme }) => theme.mixin.flexCenter}
+    ${({theme}) => theme.mixin.flexCenter}
     background-color: #2c2e48;
     border-radius: 0 6px 6px 0;
   }
--- src/Pages/Market/Market.tsx
+++ src/Pages/Market/Market.tsx
@@ -1,149 +1,141 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Routes, useNavigate } from 'react-router';
+import React, {useState, useEffect} from 'react';
+import {Route, Routes, useNavigate} from 'react-router';
 import styled from 'styled-components';
-import { Btn_Primary_FontBlack } from '../../Components/Button';
-import { IItemMarket } from '../../Store/Type/Interfaces';
+import {Btn_Primary_FontBlack} from '../../Components/Button';
+import {IItemMarket} from '../../Store/Type/Interfaces';
 import Icon_search from '../../Assets/Images/Icon_search.png';
 import Icon_cookieCharge from '../../Assets/Images/Icon_cookieCharge.png';
 import ItemCard_Market from './ItemCard_Market';
 import ModalBuying from './Modal/ModalBuying';
 import ModalCompleteBuying from './Modal/ModalCompleteBuying';
-import { getNftForSaleItems, getUserAssetInfo, searchNftForSaleItems } from '../../Api';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { marketTriggerAtom, userInfoAtom } from '../../Store/Atoms';
+import {getNftForSaleItems, getUserAssetInfo, searchNftForSaleItems} from '../../Api';
+import {useRecoilState, useRecoilValue} from 'recoil';
+import {marketTriggerAtom, userInfoAtom} from '../../Store/Atoms';
 
 const Market = () => {
-  const navigate = useNavigate();
-  const userInfo = useRecoilValue(userInfoAtom);
-  const [marketTrigger, setMarketTrigger] = useRecoilState(marketTriggerAtom);
+    const navigate = useNavigate();
+    const userInfo = useRecoilValue(userInfoAtom);
+    const [marketTrigger, setMarketTrigger] = useRecoilState(marketTriggerAtom);
 
-  const closeModal = () => {
-    navigate('/market');
-  };
+    const closeModal = () => {
+        navigate('/market');
+    };
 
-  const [isChecked, setIsChecked] = useState(false);
-  const [name, setName] = useState('');
-  const [list, setList] = useState<IItemMarket[]>([
-    {
-      id: '',
-      name: '',
-      ownerNickname: '',
-      imageURI: '',
-      maxDurability: '',
-      durability: '',
-      price: '',
-    },
-  ]);
-  const [item, setItem] = useState<IItemMarket>({
-    id: '',
-    name: '',
-    ownerNickname: '',
-    imageURI: '',
-    maxDurability: '',
-    durability: '',
-    price: '',
-  });
-  const [klay, setKlay] = useState(0);
-
-  useEffect(() => {
-    getNftForSaleItems().then((res) => {
-      console.log(res);
-      setList(res.content);
+    const [isChecked, setIsChecked] = useState(false);
+    const [name, setName] = useState('');
+    const [list, setList] = useState([] as any);
+    const [item, setItem] = useState({
+        id: '',
+        name: '',
+        ownerNickname: '',
+        imageURI: '',
+        maxDurability: '',
+        durability: '',
+        price: '',
     });
-    getUserAssetInfo(userInfo.accessToken).then((res) => {
-      console.log(res);
-      setKlay(res.content.token);
-    });
-  }, [marketTrigger]);
+    const [klay, setKlay] = useState(0);
 
-  useEffect(() => {
-    searchNftForSaleItems(userInfo.accessToken, name, isChecked).then((res) => {
-      setList(res.content);
-    });
-  }, [isChecked]);
+    useEffect(() => {
+        getNftForSaleItems().then((res) => {
+            setList(res.content);
+        });
+        getUserAssetInfo(userInfo.accessToken).then((res) => {
+            setKlay(res.content.token);
+        });
+    }, [marketTrigger]);
 
-  console.log('list', list);
-
-  return (
-    <>
-      <Container>
-        <Btn_Primary_FontBlack label="거래소" />
-        <p
-          className="my_klay"
-          onClick={() => {
-            navigate('/chargeklay');
-          }}
-        >
-          <span>{klay} KLAY</span>
-          <img src={Icon_cookieCharge} alt="charge icon" className="charge_icon" />
-        </p>
-        <div className="searchbox">
-          <img
-            src={Icon_search}
-            alt="search"
-            className="search_icon"
-            onClick={() => {
-              searchNftForSaleItems(userInfo.accessToken, name, isChecked).then((res) => {
+    useEffect(() => {
+        if (userInfo.accessToken != '') {
+            searchNftForSaleItems(userInfo.accessToken, name, isChecked).then((res) => {
                 setList(res.content);
-              });
-            }}
-          />
-          <input
-            type="text"
-            className="search"
-            placeholder="소재 이름으로 검색하기"
-            onChange={(e: any) => {
-              setName(e.target.value);
-            }}
-          />
-          <div className="checkbox_box">
-            <input
-              type="checkbox"
-              onChange={() => {
-                setIsChecked((prev) => !prev);
-              }}
-            />
-            <span>내가 판매 중인 물품 제외</span>
-          </div>
-        </div>
-        <section className="item_list">
-          {list.map((cont, idx) => (
-            <ItemCard_Market
-              key={idx}
-              id={cont.id}
-              name={cont.name}
-              durability={cont.durability}
-              maxDurability={cont.maxDurability}
-              imageURI={cont.imageURI}
-              price={cont.price ? cont.price : '0'}
-              ownerNickname={cont.ownerNickname}
-              setItem={setItem}
-            />
-          ))}
-        </section>
-      </Container>
-      <Routes>
-        <Route
-          path="/buying"
-          element={<ModalBuying closeModal={closeModal} item={item} klay={klay} />}
-        />
-        <Route path="/completebuying" element={<ModalCompleteBuying closeModal={closeModal} />} />
-      </Routes>
-    </>
-  );
+            });
+        }
+    }, [isChecked]);
+
+    return (
+        <>
+            <Container>
+                <Btn_Primary_FontBlack label="거래소"/>
+                {userInfo.accessToken != '' &&
+                <>
+                    <p
+                        className="my_klay"
+                        onClick={() => {
+                            navigate('/chargeklay');
+                        }}
+                    >
+                        <span>{klay} KLAY</span>
+                        <img src={Icon_cookieCharge} alt="charge icon" className="charge_icon"/>
+                    </p>
+                    <div className="searchbox">
+                        <img
+                            src={Icon_search}
+                            alt="search"
+                            className="search_icon"
+                            onClick={() => {
+                                searchNftForSaleItems(userInfo.accessToken, name, isChecked).then((res) => {
+                                    setList(res.content);
+                                });
+                            }}
+                        />
+                        <input
+                            type="text"
+                            className="search"
+                            placeholder="소재 이름으로 검색하기"
+                            onChange={(e: any) => {
+                                setName(e.target.value);
+                            }}
+                        />
+                        <div className="checkbox_box">
+                            <input
+                                type="checkbox"
+                                onChange={() => {
+                                    setIsChecked((prev) => !prev);
+                                }}
+                            />
+                            <span>내가 판매 중인 물품 제외</span>
+                        </div>
+                    </div>
+                </>
+                }
+                <section className="item_list">
+                    {list.map((cont, idx) => (
+                        <ItemCard_Market
+                            key={idx}
+                            id={cont.id}
+                            name={cont.name}
+                            durability={cont.durability}
+                            maxDurability={cont.maxDurability}
+                            imageURI={cont.imageURI}
+                            price={cont.price ? cont.price : '0'}
+                            ownerNickname={cont.ownerNickname}
+                            setItem={setItem}
+                        />
+                    ))}
+                </section>
+            </Container>
+            <Routes>
+                <Route
+                    path="/buying"
+                    element={<ModalBuying closeModal={closeModal} item={item} klay={klay}/>}
+                />
+                <Route path="/completebuying" element={<ModalCompleteBuying closeModal={closeModal}/>}/>
+            </Routes>
+        </>
+    );
 };
 
 export default Market;
 
 const Container = styled.div`
-  ${({ theme }) => theme.mixin.paddingSide_depth1}
-  ${({ theme }) => theme.mixin.paddingTopBottom}
+  ${({theme}) => theme.mixin.paddingSide_depth1}
+  ${({theme}) => theme.mixin.paddingTopBottom}
   > button {
     height: 44px;
   }
   .my_klay {
     color: #a2a4b7;
-    ${({ theme }) => theme.mixin.textStyle.R_12}
+    ${({theme}) => theme.mixin.textStyle.R_12}
     margin-top: 15px;
     border-radius: 15px;
     border: 1px solid #fff;
@@ -165,7 +157,7 @@ const Container = styled.div`
     .search {
       width: 100%;
       background-color: #24253a;
-      ${({ theme }) => theme.mixin.textStyle.R_16}
+      ${({theme}) => theme.mixin.textStyle.R_16}
       padding: 8px 22px;
       border-radius: 20px;
     }
@@ -182,7 +174,7 @@ const Container = styled.div`
       right: 0;
       display: flex;
       align-items: center;
-      ${({ theme }) => theme.mixin.textStyle.R_12}
+      ${({theme}) => theme.mixin.textStyle.R_12}
       color: #c2c2c2;
       height: 20px;
       span {
